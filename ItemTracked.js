bd96eeb6f0b673cf559330355dd585496a0a80b0
Dmytro Kushnir <dmytro.kushnir@globallogic.com>
2022-10-03 19:22:20 +0300

    Add support for IIFE bundle, revert uuid to get polyfill
    Co-authored-by: Dmytro Kushnir <dimakush1@gmail.com>

--- ItemTracked.js
+++ ItemTracked.js
@@ -1,4 +1,4 @@
-const { v4: uuidv4 } = require('uuid');
+var uuidv4 = require('uuid/v4');
 const { computeBearingIn360 } = require('./utils');
 const { computeVelocityVector } = require('./utils');
 
